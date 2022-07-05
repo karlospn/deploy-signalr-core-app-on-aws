02bdcb8cf05c5ac10667dc3f9b7d003f157e1566
Carlos Pons <karlospn@gmail.com>
2022-07-05 11:35:35 +0200

    Add initial files

---
+++ src/ChatRoom/Hub/ChatSampleHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ChatRoom.Hub
+{
+    public class ChatSampleHub : Microsoft.AspNetCore.SignalR.Hub
+    {
+        public void BroadcastMessage(string name, string message)
+        {
+            Clients.All.SendAsync("broadcastMessage", name, message);
+        }
+
+        public void Echo(string name, string message)
+        {
+            Clients.Client(Context.ConnectionId).SendAsync("echo", name, message + " (echo from server)");
+        }
+    }
+}
---
+++ src/ChatRoom/Startup.cs
@@ -0,0 +1,32 @@
+using ChatRoom.Hub;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace ChatRoom
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSignalR();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapHub<ChatSampleHub>("/chat");
+            });
+        }
+    }
+}
