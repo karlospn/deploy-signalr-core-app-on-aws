a1fb24cf5847e4435b5439093d92cd3682480555
Carlos Pons <karlospn@gmail.com>
2022-07-13 11:45:02 +0200

    Update README

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # How to deploy a SignalR Core application on AWS
 
-@Work in progress.
+This repository contains an example about how you can deploy a SignalR Core App on AWS.
+
+The source code for the SignalR Core app is from here:
+- https://github.com/aspnet/AzureSignalR-samples/tree/main/samples/ChatRoom
+
+In the ``/cdk`` folder you can find an AWS CDK app that creates the following infrastructure on AWS:
+
+- A VPC with ``10.55.0.0/16`` CIDR range.
+- An Application Load Balancer.
+- A Fargate service.
+- A ElasticCache instance.
+- A NAT Gateway.
+
+![diagram](https://raw.githubusercontent.com/karlospn/deploy-signalr-core-app-on-aws/main/docs/signalr_core_application.png)
--- docs/diagram.py
+++ docs/diagram.py
@@ -5,14 +5,20 @@ from diagrams.aws.network import ALB
 from diagrams.aws.database import ElasticacheForRedis
 from diagrams.aws.network import NATGateway
 
-with Diagram("SignalR Core Application", show=False):
-    
-    source = ALB("lb")
-    
-    workers = [Fargate("worker1"),
-                Fargate("worker2"),
-                Fargate("worker3")]
 
-    cache = ElasticacheForRedis("backplane")
+with Diagram("SignalR Core Application", show=False):
+
+    with Cluster("VPC"):    
+        with Cluster("public network"):
+            source = ALB("lb")
+            
+        with Cluster("private network"):
+            with Cluster("fargate workers"):
+                workers = [Fargate("worker1"),
+                            Fargate("worker2"),
+                            Fargate("worker3")]
+
+            cache = ElasticacheForRedis("backplane")
+            gw = NATGateway("nat gw")
 
     source >> workers >> cache
